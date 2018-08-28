e53559572dca0797d7586cea6deb20bddc058a72
Studio Scholz <dev@studioscholz.info>
2018-08-28 11:26:10 +0200

    Revert "composer.json and package.json added"
    This reverts commit a539b64e7776a4c2b35ea94ba91a2b8f1f222599.

--- vendor/getkirby/cms/config/fields/radio.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'mixins' => ['options'],
-    'validations' => [
-        'required',
-        'option'
-    ]
-];
--- vendor/getkirby/cms/tests/Cms/fixtures/models/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class TestPage extends Page
-{
-
-}
