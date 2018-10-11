419ad888cfdd2b019940af76ae04123036cfc9bd
robinscholz <dev@studioscholz.info>
2018-10-11 09:04:44 +0200

    Content

--- site/config/config.php
+++ site/config/config.php
@@ -29,10 +29,11 @@ return [
 
 	            return $kirby->api()->render($path, $this->method(), [
 	                'body'    => $request->body()->toArray(),
-	                'files'   => $request->files()->toArray(),
+	                // 'files'   => $request->files()->toArray(),
 	                'headers' => $request->headers(),
 	                'query'   => $request->query()->toArray(),
-	            ]);
+				]);
+				
 	        }
 	    ]
   	]
