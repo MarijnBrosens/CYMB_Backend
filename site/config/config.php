33ca805f94c4ad77d1eebd0be14dee37f3613cc4
Robin Scholz <dev@studioscholz.info>
2018-10-14 21:15:53 +0200

    api allow origin tryouts

--- site/config/config.php
+++ site/config/config.php
@@ -3,38 +3,40 @@
 return [
     'debug'  => true,
     'routes' => [
-	    [
-	        'pattern' => 'rest/(:all)',
-	        'method'  => 'GET',
-	        'env'     => 'api',
-	        'action'  => function ($path = null) {
+        [
+            'pattern' => 'rest/(:all)',
+            'method'  => 'GET',
+            'env'     => 'api',
+            'action'  => function ($path = null) {
+                $origin = $_SERVER['HTTP_ORIGIN'];
+                $allowed_domains = [
+                    'http://localhost:8080',
+                    'https://constantinmirbach.com',
+                    'https://www.constantinmirbach.com'
+                ];
+                
+                if (in_array($origin, $allowed_domains)) {
+                    header("Access-Control-Allow-Origin: " . $origin);
+                }
+                
+                // header("Access-Control-Allow-Origin: *");
+                                
+                $kirby = new Kirby();
+                $kirby->impersonate('api@connymirbach.de');
 
-	        	$origin = $_SERVER['HTTP_ORIGIN'];
-				$allowed_domains = [
-				    'http://localhost:8080'
-				];
+                if ($kirby->option('api') === false) {
+                    return null;
+                }
 
-				if (in_array($origin, $allowed_domains)) {
-				    header('Access-Control-Allow-Origin: ' . $origin);
-				}
-
-				$kirby = new Kirby();
-				$kirby->impersonate('api@connymirbach.de');
-
-	            if ($kirby->option('api') === false) {
-	                return null;
-	            }
-
-	            $request = $kirby->request();
-
-	            return $kirby->api()->render($path, $this->method(), [
-	                'body'    => $request->body()->toArray(),
-	                'files'   => $request->files()->toArray(),
-	                'headers' => $request->headers(),
-	                'query'   => $request->query()->toArray(),
-				]);
-				
-	        }
-	    ]
-  	]
+                $request = $kirby->request();
+                                
+                return $kirby->api()->render($path, $this->method(), [
+                    'body'    => $request->body()->toArray(),
+                    'files'   => $request->files()->toArray(),
+                    'headers' => $request->headers(),
+                    'query'   => $request->query()->toArray(),
+                ]);
+            }
+        ]
+    ]
 ];
