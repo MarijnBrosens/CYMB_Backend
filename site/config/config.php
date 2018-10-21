34a3ffc48be9239731850a15b1b03f957d774fc0
Robin Scholz <dev@studioscholz.info>
2018-10-21 20:12:05 +0200

    api update

--- site/config/config.php
+++ site/config/config.php
@@ -8,34 +8,52 @@ return [
             'method'  => 'GET',
             'env'     => 'api',
             'action'  => function ($path = null) {
+                $kirby = new Kirby();
+                $kirby->impersonate('api@constantinmirbach.com');
+
+                if ($kirby->option('api') === false) {
+                    return null;
+                }
+
                 $origin = $_SERVER['HTTP_ORIGIN'];
                 $allowed_domains = [
                     'http://localhost:8080',
                     'https://constantinmirbach.com',
                     'https://www.constantinmirbach.com'
                 ];
-                
-                if (in_array($origin, $allowed_domains)) {
-                    header("Access-Control-Allow-Origin: " . $origin);
-                }
-                
-                // header("Access-Control-Allow-Origin: *");
-                                
-                $kirby = new Kirby();
-                $kirby->impersonate('api@connymirbach.de');
-
-                if ($kirby->option('api') === false) {
-                    return null;
-                }
 
                 $request = $kirby->request();
-                                
-                return $kirby->api()->render($path, $this->method(), [
+                $headers = $request->headers();
+
+                if (in_array($origin, $allowed_domains)) {
+                    $headers['Access-Control-Allow-Origin'] = $origin;
+                }
+
+                $render = $kirby->api()->render($path, $this->method(), [
                     'body'    => $request->body()->toArray(),
                     'files'   => $request->files()->toArray(),
-                    'headers' => $request->headers(),
+                    'headers' => $headers,
                     'query'   => $request->query()->toArray(),
                 ]);
+
+                $decoded = json_decode($render, true);
+
+                function kt($array)
+                {
+                    foreach ($array as $key => $value) {
+                        if (is_array($value)) {
+                            $array[$key] = kt($value);
+                        } else {
+                            $array[$key] = kirbytags($value);
+                        }
+                    }
+                    return $array;
+                }
+
+                $decoded = kt($decoded);
+                $encoded = json_encode($decoded);
+
+                return $decoded;
             }
         ]
     ]
