dc1c3e399ac2a24d0e78c134d81d578c27d72bd0
Robin Scholz <dev@studioscholz.info>
2018-11-28 20:43:11 +0100

    kirby update

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -14,6 +14,10 @@ class ComposerStaticInit3734cfa62ae08f048620506fef5333b6
     );
 
     public static $prefixLengthsPsr4 = array (
+        'Z' => 
+        array (
+            'Zend\\Escaper\\' => 13,
+        ),
         'W' => 
         array (
             'Whoops\\' => 7,
@@ -38,6 +42,10 @@ class ComposerStaticInit3734cfa62ae08f048620506fef5333b6
     );
 
     public static $prefixDirsPsr4 = array (
+        'Zend\\Escaper\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/zendframework/zend-escaper/src',
+        ),
         'Whoops\\' => 
         array (
             0 => __DIR__ . '/..' . '/filp/whoops/src/Whoops',
